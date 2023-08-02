71dfab8e9c9f293a231351b74f164146f3f45439
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-08-02 12:17:13 +0530

    Update test.groovy

--- var/test.groovy
+++ var/test.groovy
@@ -1,22 +1,12 @@
-import com.tothenew.utility
+
 
 def call(String name){
-
-    object = new  utility()
-
-    pipeline{
-      agent any
-      stage('print'){
-        steps{
-            object.print(${name})
-        }
+   sh '''echo ${name}'''
+    
+    
       }
 
 
 
 
 
-    }
-     
-    
-}
